314620a23ab25a0fde055696c85d0a854838aeda
trsohankumar <sohankumartr@gmial.com>
2020-09-18 12:01:56 +0530

    Added display function

--- dummy.cpp
+++ dummy.cpp
@@ -13,11 +13,44 @@ struct info
     string album;
     string song;
     string genre;
-};
+} detail[100];
+
+void display()
+{
+    ifstream fin;
+    fin.open("dummy.csv");
+    string read, id, artist, album, genre, song;
+    bool notfound = true;
+    if (!fin)
+    {
+        cout << "file could not be opened" << endl;
+    }
+    else
+    {
+        int i = 0, size = 0;
+        while (!fin.eof())
+        {
+            getline(fin, detail[i].song, ',');
+            getline(fin, detail[i].artist, ',');
+            getline(fin, detail[i].album, ',');
+            getline(fin, detail[i].genre, '\n');
+            i++;
+            size++;
+        }
+        cout<<"artist\t\talbum\t\tsong\t\t\tgenre"<<endl;
+        for (i = 0; i < size; i++)
+        {
+            cout << detail[i].artist<< "\t\t"<<detail[i].album<< "\t\t" << detail[i].song<< "\t"<< detail[i].genre << endl;
+        }
+    }
+
+    fin.close();
+}
 
 int main()
 {
-    string search;
+    display();
+    /*string search;
     cout << "Enter the name of the song to be searched for:" << endl;
     cin >> search;
     ifstream fin;
@@ -30,6 +63,7 @@ int main()
     }
     else
     {
+        int i = 0, count = 0;
         while (!fin.eof())
         {
             getline(fin, read, ',');
@@ -62,6 +96,6 @@ int main()
         }
     }
 
-    fin.close();
+    fin.close();*/
     return 0;
 }
